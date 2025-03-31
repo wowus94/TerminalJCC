460b5b84f5d768b9d1a5af90b445b4edd0e12608
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2025-03-31 21:36:09 +0300

    Drawing graph

--- app/src/main/java/ru/shevrus/terminaljcc/presentation/MainActivity.kt
+++ app/src/main/java/ru/shevrus/terminaljcc/presentation/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
                 val screenState = viewModel.state.collectAsState()
                 when (val currentState = screenState.value) {
                     is TerminalScreenState.Content -> {
-                        Log.d("MainActivity", currentState.barList.toString())
+                        Terminal(currentState.barList)
                     }
 
                     is TerminalScreenState.Initial -> {
