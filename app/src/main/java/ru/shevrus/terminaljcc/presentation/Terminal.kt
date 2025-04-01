a2ee63d671a3490466e7e39d4c88b4b3ee15aa95
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2025-04-01 22:35:59 +0300

    Path Effect

--- app/src/main/java/ru/shevrus/terminaljcc/presentation/Terminal.kt
+++ app/src/main/java/ru/shevrus/terminaljcc/presentation/Terminal.kt
@@ -5,14 +5,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.TransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.dp
 import ru.shevrus.terminaljcc.data.Bar
 import kotlin.math.roundToInt
 
@@ -41,6 +45,7 @@ fun Terminal(bars: List<Bar>) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
+            .padding(top = 32.dp, bottom = 32.dp)
             .transformable(transformableState)
             .onSizeChanged {
                 terminalState = terminalState.copy(
@@ -72,5 +77,58 @@ fun Terminal(bars: List<Bar>) {
                 )
             }
         }
+        bars.firstOrNull()?.let {
+            drawPrices(
+                min = min,
+                pxPerPoint = pxPerPoint,
+                lastPrice = bars.first().close
+            )
+        }
     }
+}
+
+private fun DrawScope.drawPrices(
+    min: Float,
+    pxPerPoint: Float,
+    lastPrice: Float
+) {
+    // max
+    drawDashedLine(
+        start = Offset(0f, 0f),
+        end = Offset(size.width, 0f),
+        strokeWidth = 1f
+    )
+
+    // lastPrice
+    drawDashedLine(
+        start = Offset(0f, size.height - ((lastPrice - min) * pxPerPoint)),
+        end = Offset(size.width, size.height - ((lastPrice - min) * pxPerPoint)),
+        strokeWidth = 1f
+    )
+
+    // min
+    drawDashedLine(
+        start = Offset(0f, size.height),
+        end = Offset(size.width, size.height),
+        strokeWidth = 1f
+    )
+}
+
+private fun DrawScope.drawDashedLine(
+    color: Color = Color.White,
+    start: Offset,
+    end: Offset,
+    strokeWidth: Float = 1f
+) {
+    drawLine(
+        color = color,
+        start = start,
+        end = end,
+        strokeWidth = strokeWidth,
+        pathEffect = PathEffect.dashPathEffect(
+            intervals = floatArrayOf(
+                4.dp.toPx(), 4.dp.toPx()
+            )
+        )
+    )
 }
